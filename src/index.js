d76b426ee9b07d799c008dc3d987dd4a288888ec
dmod <adarmody@gmail.com>
2018-01-18 23:20:57 -0500

    Test database_url env var

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+console.log("Test prior to PG");
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const element = (
     <div>
     <h1>Hello, world!</h1>
