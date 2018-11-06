c61b21c64b5440af407a7add55f393f4fd00e528
dmod <adarmody@gmail.com>
2018-11-05 22:55:24 -0500

    -> CoreUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,26 @@
 import React, { Component } from 'react';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './App.scss';
+
+// Containers
+import { DefaultLayout } from './containers';
+// Pages
+import { Login, Page404, Page500, Register } from './views/Pages';
+
+// import { renderRoutes } from 'react-router-config';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      fetching: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        console.log(response)
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
-
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          {this.state.fetching ? 'Fetching message from API' : this.state.message}
-        </p>
-      </div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/login" name="Login Page" component={Login} />
+          <Route exact path="/register" name="Register Page" component={Register} />
+          <Route exact path="/404" name="Page 404" component={Page404} />
+          <Route exact path="/500" name="Page 500" component={Page500} />
+          <Route path="/" name="Home" component={DefaultLayout} />
+        </Switch>
+      </HashRouter>
     );
   }
 }
