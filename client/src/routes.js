ee1dfd4f63879b6c68beb2d9e898c5f53fb7b10b
dmod <adarmody@gmail.com>
2019-02-18 20:04:23 -0500

    Code cleaup & install mongodb

--- client/src/routes.js
+++ client/src/routes.js
@@ -17,31 +17,6 @@ const Dashboard = Loadable({
   loading: Loading,
 });
 
-const Colors = Loadable({
-  loader: () => import('./views/Theme/Colors'),
-  loading: Loading,
-});
-
-const Typography = Loadable({
-  loader: () => import('./views/Theme/Typography'),
-  loading: Loading,
-});
-
-const Widgets = Loadable({
-  loader: () => import('./views/Widgets/Widgets'),
-  loading: Loading,
-});
-
-const Users = Loadable({
-  loader: () => import('./views/Users/Users'),
-  loading: Loading,
-});
-
-const User = Loadable({
-  loader: () => import('./views/Users/User'),
-  loading: Loading,
-});
-
 const VCalls = Loadable({
   loader: () => import('./views/VCalls'),
   loading: Loading,
@@ -57,12 +32,6 @@ const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
   { path: '/login', name: 'Login', component: Login },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/theme', exact: true, name: 'Theme', component: Colors },
-  { path: '/theme/colors', name: 'Colors', component: Colors },
-  { path: '/theme/typography', name: 'Typography', component: Typography },
-  { path: '/widgets', name: 'Widgets', component: Widgets },
-  { path: '/users', exact: true,  name: 'Users', component: Users },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User },
   { path: '/vcalls', name: 'VCalls', component: VCalls },
   { path: '/newcall', name: 'NewCall', component: NewCall },
 ];
